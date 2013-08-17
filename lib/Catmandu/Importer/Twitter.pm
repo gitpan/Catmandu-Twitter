836cc476152c236854de9008479dd07e6dd89ab8
Patrick Hochstenbach <patrick.hochstenbach@gmail.com>
2013-08-17 12:17:36 +0000

    Import of HOCHSTEN/Catmandu-Twitter-0.03 from CPAN.
    gitpan-cpan-distribution: Catmandu-Twitter
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         HOCHSTEN/Catmandu-Twitter-0.03.tar.gz
    gitpan-cpan-author:       HOCHSTEN
    gitpan-cpan-maturity:     released

--- lib/Catmandu/Importer/Twitter.pm
+++ lib/Catmandu/Importer/Twitter.pm
@@ -1,27 +1,40 @@
 package Catmandu::Importer::Twitter;
 
 use Catmandu::Sane;
-use Net::Twitter;
 use Moo;
+use Net::Twitter;
 
 with 'Catmandu::Importer';
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
-has query   => (is => 'ro', required => 1);
-has twitter => (is => 'ro', init_arg => undef , lazy => 1 , builder => '_build_twitter');
+has query                       => ( is => 'ro', required => 1 );
+has twitter                     => ( is => 'ro' );
+has twitter_consumer_key        => ( is => 'ro', required => 1 );
+has twitter_consumer_secret     => ( is => 'ro', required => 1 );
+has twitter_access_token        => ( is => 'ro', required => 1 );
+has twitter_access_token_secret => ( is => 'ro', required => 1 );
 
-sub _build_twitter {
-    Net::Twitter->new();
-}
+before generator => sub {
+    my $self = shift;
+    $self->{twitter} = Net::Twitter->new(
+        traits              => [qw/API::RESTv1_1/],
+        consumer_key        => $self->twitter_consumer_key,
+        consumer_secret     => $self->twitter_consumer_secret,
+        access_token        => $self->twitter_access_token,
+        access_token_secret => $self->twitter_access_token_secret,
+    ) or die "$!";
+
+};
 
 sub generator {
     my ($self) = @_;
-    
+
     sub {
-        state $res = $self->twitter->search($self->query);
-        return unless @{$res->{results}};
-        return shift @{$res->{results}};
+        state $res = $self->{twitter}->search( $self->query );
+
+        return unless @{ $res->{statuses} };
+        return shift @{ $res->{statuses} };
     };
 }
 
@@ -33,7 +46,13 @@ Catmandu::Importer::Twitter - Package that imports Twitter feeds
 
     use Catmandu::Importer::Twitter;
 
-    my $importer = Catmandu::Importer::Twitter->new(query => '#elag2013' );
+    my $importer = Catmandu::Importer::Twitter->new(
+                        consumer_key => '<your key>' ,
+                        consumer_secret => '<your secret>' ,
+                        access_token => '<your token>' ,
+                        access_token_secret => '<your token secret>' ,    
+                        query => '#elag2013' 
+                    );
 
     my $n = $importer->each(sub {
         my $hashref = $_[0];
